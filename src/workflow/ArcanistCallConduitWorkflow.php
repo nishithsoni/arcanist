9f3b60ac1dd2ea0f1a28f63603dd9e7249efed8c
Albert Wu <albertywu@gmail.com>
2025-07-15 11:18:06 -0700

    Add more practical examples to the arc call-conduit help output (#305)
    * Add more practical examples to the arc call-conduit help output
    
    * consistent formatting
    
    * more consistent formatting
    
    * get repository by callsign
    
    * add info about conduit API token
    
    * casing
    
    * consistent colon usage
    
    ---------
    
    Co-authored-by: wua <wua+UBER@uber.com>

--- src/workflow/ArcanistCallConduitWorkflow.php
+++ src/workflow/ArcanistCallConduitWorkflow.php
@@ -24,12 +24,90 @@ EOTEXT
             - Run this command from a working directory.
             - Call parameters are REQUIRED and read as a JSON blob from stdin.
             - Results are written to stdout as a JSON blob.
+            - Requires a valid Conduit API token set in ~/.arcrc (run `arc install-certificate` to get one)
 
           This workflow is primarily useful for writing scripts which integrate
-          with Phabricator. Examples:
+          with Phabricator.
 
+          Examples:
+            
+            Ping (check connectivity):
             $ echo '{}' | arc call-conduit conduit.ping
-            $ echo '{"phid":"PHID-FILE-xxxx"}' | arc call-conduit file.download
+
+            Get Current User:
+            $ echo '{}' | arc call-conduit user.whoami
+
+            Get User Info:
+            $ echo '{"constraints":{"usernames":["wua", "foo"]}}' | arc call-conduit user.search
+
+            Search Revisions by ID:
+            $ echo '{"constraints":{"ids":[123,456,789]}}' | arc call-conduit differential.revision.search
+            
+            Search Revisions by PHID:
+            $ echo '{"constraints":{"phids":["PHID-DREV-wd4ovydtv4m5nsz6enax"]}}' | arc call-conduit differential.revision.search
+            
+            Search Revisions by User:
+            $ echo '{"constraints":{"authorPHIDs":["PHID-USER-g34zkv234n3rk3xlhgke"]}}' | arc call-conduit differential.revision.search
+
+            Update Revision Title:
+            $ echo '{"transactions":[{"type":"title","value":"my new title"}],"objectIdentifier":"PHID-DREV-zp4o4lfpsfwhnkglxln3"}' | arc call-conduit differential.revision.edit
+            
+            Update Revision Jira:
+            $ echo '{"transactions":[{"type":"uber-jira.issues","value":["CODE-204"]}],"objectIdentifier":"PHID-DREV-zp4o4lfpsfwhnkglxln3"}' | arc call-conduit differential.revision.edit
+            
+            Update Revision Accept:
+            $ echo '{"transactions":[{"type":"accept","value":true}],"objectIdentifier":"PHID-DREV-sangdbezeh5nfeicqzeg"}' | arc call-conduit differential.revision.edit
+
+            Add Projects to Revision:
+            $ echo '{"transactions":[{"type":"projects.add","value":["PHID-PROJ-u4i3446wedyolppkckbp"]}],"objectIdentifier":"PHID-DREV-zp4o4lfpsfwhnkglxln3"}' | arc call-conduit differential.revision.edit
+            
+            Remove Projects from Revision:
+            $ echo '{"transactions":[{"type":"projects.remove","value":["PHID-PROJ-u4i3446wedyolppkckbp"]}],"objectIdentifier":"PHID-DREV-zp4o4lfpsfwhnkglxln3"}' | arc call-conduit differential.revision.edit
+            
+            Set Projects on Revision:
+            $ echo '{"transactions":[{"type":"projects.set","value":["PHID-PROJ-u4i3446wedyolppkckbp"]}],"objectIdentifier":"PHID-DREV-zp4o4lfpsfwhnkglxln3"}' | arc call-conduit differential.revision.edit
+
+            Get Revision's Projects (aka Tags):
+            $ echo '{"constraints":{"ids":[13050281]},"attachments":{"projects":true}}' | arc call-conduit differential.revision.search
+            
+            Get Projects Details:
+            $ echo '{"constraints":{"phids":["PHID-PROJ-u4i3446wedyolppkckbp"]}}' | arc call-conduit project.search
+
+            Get Comments:
+            $ echo '{"objectIdentifier":"D13050281"}' | arc call-conduit transaction.search
+            
+            Create General Comment:
+            $ echo '{"revision_id":13050281,"message":"hi"}' | arc call-conduit differential.createcomment
+            
+            Create General Comment & Accept:
+            $ echo '{"revision_id":13050281,"message":"a general comment!","action":"accept"}' | arc call-conduit differential.createcomment
+            
+            Create Inline Comment:
+            $ echo '{"revisionID":13050281,"diffID":36482097,"filePath":"src/infra/devplatform/code-infra/code-review-ux/goo","isNewFile":true,"lineNumber":1,"content":"making an inline comment"}' | arc call-conduit differential.createinline
+
+            Create Inline Comment (threaded comment):
+            $ echo '{"revisionID":13050281,"diffID":36482097,"filePath":"src/infra/devplatform/code-infra/code-review-ux/goo","isNewFile":true,"lineNumber":1,"content":"making a comment thread","replyToCommentID":114219845}' | arc call-conduit differential.createinline
+
+            Get Buildable:
+            $ echo '{"constraints":{"containerPHIDs":["PHID-DREV-zp4o4lfpsfwhnkglxln3"]}}' | arc call-conduit harbormaster.buildable.search
+            
+            Get Build:
+            $ echo '{"constraints":{"buildables":["PHID-HMBB-qdryny55lxb5k3zjcpwr"]}}' | arc call-conduit harbormaster.build.search
+            
+            Get Build Targets:
+            $ echo '{"constraints":{"buildPHIDs":["PHID-HMBD-bx3g3fvgi62z5txlgnom"]},"order":"newest"}' | arc call-conduit harbormaster.target.search
+            
+            Get Build Target Logs:
+            $ echo '{"constraints":{"buildTargetPHIDs":["PHID-HMBT-cdnilgbxily6lmkss3oz"]},"order":"newest"}' | arc call-conduit harbormaster.log.search
+
+            Get File (returns file as base64-encoded string):
+            $ echo "{\"phid\":\"PHID-FILE-477xrgjyhhxd4qcurgtx\"}" | arc call-conduit file.download
+
+            Get Repository:
+            $ echo '{"constraints":{"phids":["PHID-REPO-uexvk77yeovy63fhokqw"]}}' | arc call-conduit diffusion.repository.search
+
+            Get Repository by Callsign:
+            $ echo '{"constraints":{"callsigns":["GOCODVJ"]}}' | arc call-conduit diffusion.repository.search
 EOTEXT
       );
   }
